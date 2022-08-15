6671cb483b215bbf478afc7c586de17b63ec7c18
Valokoodari <Valokoodari@users.noreply.github.com>
2022-08-15 21:30:02 +0300

    Exercise 1.1

---
+++ part-01/course-info/src/App.js
@@ -0,0 +1,44 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.parts[0]} {props.exercises[0]}</p>
+      <p>{props.parts[1]} {props.exercises[1]}</p>
+      <p>{props.parts[2]} {props.exercises[2]}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const parts = [part1, part2, part3]
+  const exercises = [exercises1, exercises2, exercises3]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises}/>
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
