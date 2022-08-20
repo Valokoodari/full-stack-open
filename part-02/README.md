d2c823020dd3789c160f4307646799a4b8de2d06
Valokoodari <Valokoodari@users.noreply.github.com>
2022-08-20 15:29:06 +0300

    Exercise 2.11

---
+++ part-02/README.md
@@ -0,0 +1,2 @@
+<h3>NOTE: I used the terms contact and contacts instead of the terms person and
+persons in the phonebook.</h3>
--- part-02/phonebook/src/App.js
+++ part-02/phonebook/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Contacts from './components/Contacts'
 import ContactForm from './components/ContactForm'
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/contacts')
+      .then(response => setContacts(response.data))
+  }, [])
 
   const handleNameChange = (event) =>
     setNewName(event.target.value)
