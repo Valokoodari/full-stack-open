359269cd7de6e12ef8a16b97560e073c9f7cacfa
Valokoodari <Valokoodari@users.noreply.github.com>
2022-08-20 12:32:59 +0300

    Exercise 2.1

---
+++ part-02/course-info/src/components/Course.js
@@ -0,0 +1,43 @@
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce((acc, curr) => {
+    return acc + curr.exercises
+  }, 0)
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
