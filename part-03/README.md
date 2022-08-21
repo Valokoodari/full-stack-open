f6054133a1e8ed2cb6138cd4aa4c4c183d7ed325
Valokoodari <Valokoodari@users.noreply.github.com>
2022-08-21 11:36:31 +0300

    Exercise 3.10

---
+++ part-03/README.md
@@ -0,0 +1,3 @@
+# Part 3
+
+[Phonebook backend info page](https://valo-phonebook-backend.herokuapp.com/info/)
--- part-03/frontend/src/services/contacts.js
+++ part-03/frontend/src/services/contacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://valo-phonebook-backend.herokuapp.com/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(response => response.data)
 
