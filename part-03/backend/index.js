b85ea7656ad65945b7b8c88d28bc9d7fed038f72
Valokoodari <Valokoodari@users.noreply.github.com>
2022-08-21 10:50:51 +0300

    Exercise 3.8

--- part-03/backend/index.js
+++ part-03/backend/index.js
@@ -2,28 +2,33 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('post-data', (req, _) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ' '
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - '
+  + ':response-time ms :post-data'))
 
 let contacts = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -42,7 +47,6 @@ app.get('/api/persons/', (_, res) => {
 
 app.post('/api/persons/', (req, res) => {
   const contact = req.body
-  contact.id = Math.floor(Math.random() * 2000000000)
 
   if (!contact.name) {
     return res.status(400).json({
@@ -58,9 +62,9 @@ app.post('/api/persons/', (req, res) => {
     })
   }
 
+  contact.id = Math.floor(Math.random() * 2000000000)
   contacts = contacts.concat(contact)
 
-  console.log(contact)
   res.json(contact)
 })
 
