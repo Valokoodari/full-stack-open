1189603328e6b67e8e62e2f733f6f91643b06c9a
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-07 12:26:11 +0300

    Exercise 4.4

--- part-04/bloglist/tests/list_helper.test.js
+++ part-04/bloglist/tests/list_helper.test.js
@@ -1,8 +1,37 @@
 const listHelper = require("../utils/list_helper")
+const testBlogs = require("./test_blogs")
 
 test("dummy returns one", () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe("total likes", () => {
+  test("of an empty list is zero", () => {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+
+  test("of a list with a single blog is the likes of that blog", () => {
+    const blogs = [
+      {
+        title: "I Love Summer",
+        author: "Olaf",
+        url: "https://frozen.com/olaf",
+        likes: 23
+      }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(23)
+  })
+
+  test("of a larger list is correct", () => {
+    const result = listHelper.totalLikes(testBlogs)
+    expect(result).toBe(36)
+  })
 })
