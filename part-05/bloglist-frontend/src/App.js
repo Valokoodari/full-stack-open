508d67d428a199e0abcabcc8535b6ed2256dd847
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-11 19:05:06 +0300

    Exercise 5.1

--- part-05/bloglist-frontend/src/App.js
+++ part-05/bloglist-frontend/src/App.js
@@ -1,15 +1,44 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react"
+import Blog from "./components/Blog"
+import LoginForm from "./components/LoginForm";
+import loginService from "./services/login"
+import blogService from "./services/blogs"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      console.log("wrong credentials")
+    }
+  }
+
+  if (!user) {
+    return (
+      <LoginForm username={username} password={password}
+        setUsername={setUsername} setPassword={setPassword}
+        handleLogin={handleLogin}
+      />
+    )
+  }
 
   return (
     <div>
