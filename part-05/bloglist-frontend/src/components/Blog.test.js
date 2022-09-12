ba3016aaf7a095364108eabfaa1576a4f324b524
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-12 14:39:13 +0300

    Exercise 5.14

--- part-05/bloglist-frontend/src/components/Blog.test.js
+++ part-05/bloglist-frontend/src/components/Blog.test.js
@@ -27,3 +27,19 @@ test("Blog component displays only the title and author by default", () => {
   expect(screen.queryByText("likes 242")).not.toBeInTheDocument()
   expect(screen.queryByText("The Snowman")).not.toBeInTheDocument()
 })
+
+test("Blog component displays all information after the view button is clicked", () => {
+  const element = render(<Blog blog={test_blog} user={test_user} createBlog={()=> {}} updateBlog={() => {}} removeBlog={() => {}} />)
+  expect(element).toBeDefined()
+
+  const button = screen.getByText("view")
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+  })
+
+  expect(screen.getByText("I Love Summer")).toBeDefined()
+  expect(screen.getByText("by Olaf")).toBeInTheDocument()
+  expect(screen.getByText("https://frozen.com/olaf")).toBeInTheDocument()
+  expect(screen.getByText("likes 242")).toBeInTheDocument()
+  expect(screen.getByText("The Snowman")).toBeInTheDocument()
+})
