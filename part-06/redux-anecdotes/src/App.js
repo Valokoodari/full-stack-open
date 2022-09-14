717e67aac548c9a1291e45ada878aa179f4d4f44
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-14 11:07:50 +0300

    Exercise 6.3

--- part-06/redux-anecdotes/src/App.js
+++ part-06/redux-anecdotes/src/App.js
@@ -1,11 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux"
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    dispatch({
+      type: "VOTE",
+      data: { id }
+    })
   }
 
   return (
--- part-06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,10 +1,10 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -20,10 +20,23 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  console.log("state now: ", state)
+  console.log("action", action)
 
-  return state
+  switch (action.type) {
+    case "VOTE":
+      const id = action.data.id
+      const anecdoteToVote = state.find(n => n.id === id)
+      const votedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      )
+    default:
+      return state
+  }
 }
 
 export default reducer
