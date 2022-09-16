35afbfc6cb62a3d10247f7358783735ccdaa8c99
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-16 13:11:23 +0300

    Part 7 - Fix blog order

--- part-07/bloglist-frontend/src/components/BlogList.js
+++ part-07/bloglist-frontend/src/components/BlogList.js
@@ -20,18 +20,20 @@ const BlogList = () => {
           </tr>
         </thead>
         <tbody>
-          {blogs.map((blog) => (
-            <tr key={blog.id}>
-              <td>
-                <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
-              </td>
-              <td>{blog.author}</td>
-              <td>
-                <a href={blog.url}>Visit</a>
-              </td>
-              <td>{blog.likes}</td>
-            </tr>
-          ))}
+          {[...blogs]
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <tr key={blog.id}>
+                <td>
+                  <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+                </td>
+                <td>{blog.author}</td>
+                <td>
+                  <a href={blog.url}>Visit</a>
+                </td>
+                <td>{blog.likes}</td>
+              </tr>
+            ))}
         </tbody>
       </Table>
     </div>
