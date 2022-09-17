5a925e0de0bd48dca4c89a2a69fe80ed28954661
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-17 23:32:31 +0300

    Exercise 8.22

--- part-08/library-frontend/src/components/Books.js
+++ part-08/library-frontend/src/components/Books.js
@@ -2,25 +2,42 @@ import { useEffect, useState } from "react";
 import { useQuery, useLazyQuery } from "@apollo/client";
 import { ALL_BOOKS, BOOKS_BY_GENRE } from "../queries";
 
-const Books = ({ show }) => {
+const Books = () => {
   const [genre, setGenre] = useState(null);
   const [getBooks, result] = useLazyQuery(BOOKS_BY_GENRE);
+  const [genres, setGenres] = useState([]);
   const books = useQuery(ALL_BOOKS);
 
   useEffect(() => {
     if (genre) {
-      getBooks({ variables: { genre } });
+      getBooks({
+        variables: { genre },
+        fetchPolicy: "cache-and-network", // update the cache when a genre is selected
+      });
     }
   }, [genre, getBooks]);
+
+  // Update the cache when all genres is selected
+  useEffect(() => {
+    if (result.data) {
+      books.refetch();
+    }
+  }, [genre, result.data, books]);
+
+  // Also update the genres list when new books are added or removed
+  useEffect(() => {
+    if (books.data) {
+      const genres = books.data.allBooks
+        .map((book) => book.genres)
+        .flat()
+        .filter((genre, index, self) => self.indexOf(genre) === index);
+      setGenres(genres);
+    }
+  }, [books.data]);
 
   if (books.loading) {
     return <div>loading...</div>;
   }
-
-  const genres = books.data.allBooks
-    .map((book) => book.genres)
-    .flat()
-    .filter((genre, index, array) => array.indexOf(genre) === index);
 
   return (
     <div>
@@ -37,7 +54,7 @@ const Books = ({ show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data
+          {result.data && genre
             ? result.data.allBooks.map((a) => (
                 <tr key={a.title}>
                   <td>{a.title}</td>
