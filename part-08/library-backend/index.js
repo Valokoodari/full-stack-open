ff6d373d8626ab1c9445f8aa506b0014437a02a0
Valokoodari <Valokoodari@users.noreply.github.com>
2022-09-17 17:39:31 +0300

    Exercise 8.4

--- part-08/library-backend/index.js
+++ part-08/library-backend/index.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -109,7 +109,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, { author }) => {
+      if (!author) {
+        return books;
+      } else {
+        return books.filter((book) => book.author === author);
+      }
+    },
     allAuthors: () => {
       return authors.map((author) => {
         return {
