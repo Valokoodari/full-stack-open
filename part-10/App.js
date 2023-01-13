c978547cb0e91d1380275332930f5599c77e9b27
Valokoodari <Valokoodari@users.noreply.github.com>
2023-01-13 14:55:09 +0200

    Exercise 10.11

--- part-10/App.js
+++ part-10/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
+import { ApolloProvider } from "@apollo/client";
 import { NativeRouter } from "react-router-native";
+import createApolloClient from "./src/utils/apolloClient";
 import Main from "./src/components/Main";
 
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
   <>
     <NativeRouter>
-      <Main />
+      <ApolloProvider client={apolloClient}>
+        <Main />
+      </ApolloProvider>
     </NativeRouter>
     <StatusBar style="light" />
   </>
---
+++ part-10/src/hooks/useRepositories.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  useEffect(() => {
+    if (data) {
+      setRepositories(data.repositories);
+    }
+  }, [data]);
+
+  return { repositories, loading, error };
+}
+
+export default useRepositories;
