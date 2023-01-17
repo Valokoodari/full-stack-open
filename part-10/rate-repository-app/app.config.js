2ca0c0cc9bd6665155f1d5817128781ed7f20272
Valokoodari <Valokoodari@users.noreply.github.com>
2023-01-17 14:36:50 +0200

    Part 10 - Preparations for Android build

--- part-10/rate-repository-app/app.config.js
+++ part-10/rate-repository-app/app.config.js
@@ -4,5 +4,11 @@ export default {
   name: "rate-repository-app",
   extra: {
     apolloUri: process.env.APOLLO_URI,
-  }
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID,
+    },
+  },
+  android: {
+    package: "com.valokoodari.rateRepositoryApp",
+  },
 };
